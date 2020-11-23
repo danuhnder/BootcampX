46565d85473e6357fed38a0127aea3613d5d064e
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-23 20:01:21 +0000

    add rollover students query

---
+++ 1_queries/7_students_with_start_dates_that_differ_from_cohort_start_date.sql
@@ -0,0 +1,3 @@
+SELECT students.name AS student_name, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date, students.start_date AS student_start_date
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE students.start_date <> cohorts.start_date;
