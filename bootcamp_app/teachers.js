fe69a40e0ef1f868848b2b4bbc911a62170b3355
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-26 12:39:48 +0000

    sanitise data input in teachers.js and students.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,21 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name 
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}' 
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher; 
+`
+const values = [`${process.argv[2]}`]
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(teacher => {
-    console.log(`${process.argv[2]}: ${teacher.name}`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));;
 
